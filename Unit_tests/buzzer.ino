222521185b77a2284214ffed8e1accc9f578bc8c
LiamTouier <165029796+LiamTouier@users.noreply.github.com>
2024-04-16 15:07:08 +0200

    Update buzzer.ino

--- Unit_tests/buzzer.ino
+++ Unit_tests/buzzer.ino
@@ -1,14 +1,17 @@
-int buzzer; 
+// définir les pin utilisés par le buzzer et le bouton stop
+int buzzer = 5; 
+int stop_button_pin = 2;
 
+// définir les pin utilisés par les moteurs
 #define left_motor_pin_fwd 6
 #define left_motor_pin_bck 7
 #define right_motor_pin_fwd 8
 #define right_motor_pin_bck 9
 
-
 void setup(){
    Serial.begin(9600);
    pinMode(buzzer, OUTPUT);
+   pinMode(stop_button_pin, INPUT_PULLUP);
    pinMode(left_motor_pin_fwd, OUTPUT);
    pinMode(left_motor_pin_bck, OUTPUT);
    pinMode(right_motor_pin_fwd, OUTPUT);
@@ -17,16 +20,19 @@ void setup(){
 
 
 void loop(){
-   // allume le buzzer avec frequence de 500Hz
-   tone(buzzer, 500); 
-   delay(5000);
-   // stopper le moteur 
-   analogWrite(left_motor_pin_fwd, 0);
-   analogWrite(left_motor_pin_bck, 0);
-   analogWrite(right_motor_pin_fwd, 0);
-   analogWrite(right_motor_pin_bck, 0);
-   
-   // éteins le buzzer
-   noTone(buzzer);
-   delay(500);
+   if (digitalRead(stop_button_pin) == LOW) {
+      // allumer le buzzer avec une fréquence de 500Hz pendant 5s
+      tone(buzzer, 500); 
+      delay(5000);
+
+      // arrêter les moteurs 
+      analogWrite(left_motor_pin_fwd, 0);
+      analogWrite(left_motor_pin_bck, 0);
+      analogWrite(right_motor_pin_fwd, 0);
+      analogWrite(right_motor_pin_bck, 0);
+      
+      // éteindre le buzzer
+      noTone(buzzer);
+      delay(500);
+   }
 }
