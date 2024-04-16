847a81a533d2826838c8e86446f84d2fff751a83
LiamTouier <165029796+LiamTouier@users.noreply.github.com>
2024-04-16 14:51:40 +0200

    Update buzzer.ino

--- Unit_tests/buzzer.ino
+++ Unit_tests/buzzer.ino
@@ -1,16 +1,31 @@
 int buzzer; 
 
+#define left_motor_pin_fwd 6
+#define left_motor_pin_bck 7
+#define right_motor_pin_fwd 8
+#define right_motor_pin_bck 9
+
+
 void setup(){
    Serial.begin(9600);
    pinMode(buzzer, OUTPUT);
+   pinMode(left_motor_pin_fwd, OUTPUT);
+   pinMode(left_motor_pin_bck, OUTPUT);
+   pinMode(right_motor_pin_fwd, OUTPUT);
+   pinMode(right_motor_pin_bck, OUTPUT);
 }
+
 
 void loop(){
    // allume le buzzer avec frequence de 500Hz
    tone(buzzer, 500); 
    delay(5000);
    // stopper le moteur 
-   digitalWrite(moteur, LOW);
+   analogWrite(left_motor_pin_fwd, 0);
+   analogWrite(left_motor_pin_bck, 0);
+   analogWrite(right_motor_pin_fwd, 0);
+   analogWrite(right_motor_pin_bck, 0);
+   
    // éteins le buzzer
    noTone(buzzer);
    delay(500);
