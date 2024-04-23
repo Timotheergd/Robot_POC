95affc724fc213d9f2c0a22b3bc905dcc049fbfe
LiamTouier <165029796+LiamTouier@users.noreply.github.com>
2024-04-23 14:39:48 +0200

    Update obstacle.ino

--- Unit_tests/obstacle.ino
+++ Unit_tests/obstacle.ino
@@ -1,8 +1,13 @@
 #include "Ultrasonic.h"
-#define DISTANCE_SEUIL 5 // seuil de 5cm
+#include "moteur2.ino"
+#define DISTANCE_SEUIL 20 // seuil de 20cm
 
 Ultrasonic ultrasonic(2);
 
+// définir les pin utilisés par le buzzer 
+int buzzer = 10; 
+
+// définir les pin utilisés par les moteurs
 #define left_motor_pin_fwd 6
 #define left_motor_pin_bck 7
 #define right_motor_pin_fwd 8
@@ -13,11 +18,17 @@ Ultrasonic ultrasonic(2);
 // #define PWM_SPEED(speed) (speed-10)*25 // Theorique
 #define PWM_SPEED(speed) (speed*2)-256
 
+#define TPS_VIRAGE1 2000
+#define TPS_TT_DROIT 6000
+#define TPS_VIRAGE2 8000
 
+
+unsigned long begin_turn = millis();
 
 
 void setup(){
     Serial.begin(9600);
+    pinMode(buzzer, OUTPUT);
     pinMode(left_motor_pin_fwd, OUTPUT);
     pinMode(left_motor_pin_bck, OUTPUT);
     pinMode(right_motor_pin_fwd, OUTPUT);
@@ -25,59 +36,48 @@ void setup(){
 }
 
 void loop(){
-  avoidObstacle(128, "f");
+  avoidObstacle(128, "F", &begin_turn, &turning);
 }
 
 
 
-void avoidObstacle(int speedy, char dir){
+void avoidObstacle(int speedy, char dir, unsigned long begin_turn, bool *turning){
+
+    static unsigned long last_buzz_time = 0; 
+    static int buzz_count = 0; 
+    
     int speed = PWM_SPEED(speedy);
     int RangeInCentimeters = ultrasonic.MeasureInCentimeters();
+    unsigned long current_time = millis(); // Temps actuel
 
+    //faire 5 bips toutes les secondes
+    vincentlagaf();
+        
+   /* analogWrite(right_motor_pin_fwd, speed+offset_motor_left);
+    analogWrite(right_motor_pin_bck, 0);
+    // att qu'on tourne à droite de 90° ou 45°
+    */
+    
     if (RangeInCentimeters <= DISTANCE_SEUIL){
-        //stopper moteurs
-        analogWrite(left_motor_pin_fwd, 0);
-        analogWrite(left_motor_pin_bck, 0);
-        analogWrite(right_motor_pin_fwd, 0);
-        analogWrite(right_motor_pin_bck, 0);
-    
-        delay(500);
-    
-        //tourner dans une direction ayant une distance superieur au DISTANCE_SEUIL
-        if(dir=="r" || dir=="f"){
-              Serial.println("forward right");
-              analogWrite(left_motor_pin_fwd, speed+offset_motor_left);
-              analogWrite(left_motor_pin_bck, 0);
+   
+        if (millis()-*begin_turn<TPS_VIRAGE1){
+            if (!turning) begin_turn = millis(){
+                *turning=true;
+                go(speed, "R");
+            }
+
+        else if(millis()-*begin_turn<TPS_TT_DROIT){
+            go(speed, "F");
         }
-        else if(dir=="l" || dir=="f"){
-              Serial.println("forward left");
-              analogWrite(right_motor_pin_fwd, speed);
-              analogWrite(right_motor_pin_bck, 0);
-        }
-        else if(dir=="f" || RangeInCentimeters <= DISTANCE_SEUIL){
-              Serial.println("forward left");
-              analogWrite(right_motor_pin_fwd, speed);
-              analogWrite(right_motor_pin_bck, 0);
+            
+        else if(millis()-*begin_turn<TPS_VIRAGE2){
+            go(speed, "R");
         }
         else{
-            analogWrite(left_motor_pin_fwd, 0);
-            analogWrite(left_motor_pin_bck, -(speed+offset_motor_left));
-            analogWrite(right_motor_pin_fwd, 0);
-            analogWrite(right_motor_pin_bck, -speed);
-        }}
-    else{
-            //continuer tout droit
-            analogWrite(left_motor_pin_fwd, speed + offset_motor_left);
-            analogWrite(left_motor_pin_bck, 0);
-            analogWrite(right_motor_pin_fwd, speed);
-            analogWrite(right_motor_pin_bck, 0);
+            *turning=false;
+        }
     }
+    
 }
-}
-
-
-
-
-
 
 
